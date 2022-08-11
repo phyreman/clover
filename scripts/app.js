4c6bbffaacf32126395cf0964e918c6344beddf3
John-Michael Glenn <phyre19@gmail.com>
2022-08-11 16:48:42 -0700

    v0.3.0
    Added ability to crop the image before processing it.
    Tweaked some functions to check for a TextDetector first, then fallback to Tesseract if not available.

--- scripts/app.js
+++ scripts/app.js
@@ -25,15 +25,6 @@
 	}
 
 	const canDetect = !!window.TextDetector,
-				constraints = {
-					video: {
-						aspectRatio: 16/9,
-						facingMode: "environment",
-						width: screen.height,
-						zoom: 1
-					},
-					audio: false
-				},
 				detector = canDetect && new TextDetector(),
 				worker = !canDetect && Tesseract.createWorker({
 					workerPath: "tesseract/worker.min.js",
@@ -43,9 +34,26 @@
 						wait_status.textContent = p.status;
 						load_meter.value = p.progress;
 					}
-				}),
-				ctx = canvas.getContext("2d");
-	let track;
+				});
+
+	if (!(detector || worker)) {
+		alert("Failed to create a valid detector, aborting...");
+		return;
+	}
+
+	const constraints = {
+					video: {
+						aspectRatio: 16/9,
+						facingMode: "environment",
+						width: screen.height,
+						zoom: 1
+					},
+					audio: false
+				},
+				ctx = canvas.getContext("2d"),
+				scale = 2;
+
+	ctx.scale(scale, scale);
 
 	if (worker) {
 		wait.showModal();
@@ -57,8 +65,9 @@
 		wait.close();
 		load_meter.max = 1;
 		load_meter.value = 0;
-		ctx.scale(3, 3);
 	}
+
+	let track, offset, lastPoint, isDragging = false;
 
 	// Connect to the camera
 	navigator.mediaDevices.getUserMedia(constraints)
@@ -91,44 +100,123 @@
 
 	cam.addEventListener("play", event => {
 		cam.style.marginLeft = `calc(50vw - ${cam.videoWidth / 2}px)`;
-		canvas.width = cam.videoWidth * 3;
-		canvas.height = cam.videoHeight * 3;
+		offset = getOffset(cam);
+		canvas.width = cam.videoWidth * scale;
+		canvas.height = cam.videoHeight * scale;
 	}, { once: true });
 
 	cam.addEventListener("click", scan);
 
 	output_dialog.addEventListener("close", function(event) {
+		cropper.hidden = true;
+		crop_controls.hidden = true;
 		cam.play();
-		if (this.returnValue === "") return;
-		if (this.returnValue === "default") {
-			// Form was submitted
-			saveAs(output.textContent, "output.txt");
-		}
+		this.returnValue === "default" && saveAs(output.textContent, "output.txt");
 	});
 
-	async function scan() {
+	cropper.addEventListener("mousedown", handleMouseDown);
+	cropper.addEventListener("touchstart", handleMouseDown);
+	document.body.addEventListener("mousemove", handleMouseMove);
+	document.body.addEventListener("touchmove", handleMouseMove, { passive: false });
+	cropper.addEventListener("mouseup", handleMouseUp);
+	cropper.addEventListener("touchend", handleMouseUp);
+
+	crop.addEventListener("click", async event => {
+		const { left, top, height, width } = cropper.getBoundingClientRect();
+		if (detector) {
+			canvas.width = width * scale;
+			canvas.height = height * scale;
+			ctx.drawImage(cam, left - offset.x, top - offset.y, width, height, 0, 0, canvas.width, canvas.height);
+			detector
+			.detect(canvas)
+			.then(texts => {
+				if (texts.length < 1) return;
+				output.textContent = texts.map(text => text.rawValue).join("\r\n");
+				output_dialog.showModal();
+			}).catch(err => alert(err));
+			return;
+		}
+		ctx.drawImage(cam, 0, 0, canvas.width, canvas.height);
+		wait.showModal();
+		const { data: { text } } = await worker.recognize(canvas, { rectangle: { left: left - offset.x, top: top - offset.y, height: height * scale, width: width * scale } });
+		wait.close();
+		load_meter.value = 0;
+		if (!text) return;
+		output.textContent = text;
+		output_dialog.showModal();
+	});
+	nocrop.addEventListener("click", async event => {
 		if (detector) {
 			detector
 			.detect(cam)
 			.then(texts => {
 				if (texts.length < 1) return;
 				output.textContent = texts.map(text => text.rawValue).join("\r\n");
-				cam.pause();
 				output_dialog.showModal();
 			}).catch(err => alert(err));
+			return;
 		}
-		if (worker) {
-			// const { data: { text } } = await worker.recognize(cam);
-			ctx.drawImage(cam, 0, 0, canvas.width, canvas.height);
-			cam.pause();
-			wait.showModal();
-			const { data: { text } } = await worker.recognize(canvas);
-			wait.close();
-			load_meter.value = 0;
-			if (!text) return;
-			output.textContent = text;
-			output_dialog.showModal();
+		ctx.drawImage(cam, 0, 0, canvas.width, canvas.height);
+		wait.showModal();
+		const { data: { text } } = await worker.recognize(canvas);
+		wait.close();
+		load_meter.value = 0;
+		if (!text) return;
+		output.textContent = text;
+		output_dialog.showModal();
+	});
+
+	function getOffset(element) {
+		let x = 0, y = 0;
+
+		if (element.offsetParent !== undefined) {
+			do {
+				x += element.offsetLeft;
+				y += element.offsetTop;
+			} while ((element = element.offsetParent));
 		}
+
+		return { x, y };
+	}
+
+	function getMouse(e) {
+		return {
+			x: (e.pageX || e.touches[0].clientX || 0) - offset.x,
+			y: (e.pageY || e.touches[0].clientY || 0) - offset.y
+		};
+	}
+
+	function handleMouseDown(e) {
+		lastPoint = getMouse(e);
+		const x = lastPoint.x - (cropper.offsetLeft - offset.x),
+					y = lastPoint.y - cropper.offsetTop,
+					{ width, height } = cropper.getBoundingClientRect();
+		if (!(x >= width - 19 && y >= height - 19)) isDragging = true;
+	}
+
+	function handleMouseMove(e) {
+		if (!isDragging) return;
+
+		e.preventDefault();
+
+		const currentPoint = getMouse(e),
+					x = lastPoint.x - currentPoint.x,
+					y = lastPoint.y - currentPoint.y;
+		lastPoint = currentPoint;
+
+		cropper.style.left = `${cropper.offsetLeft - x}px`;
+		cropper.style.top = `${cropper.offsetTop - y}px`;
+	}
+
+	function handleMouseUp(e) {
+		isDragging = false;
+		lastPoint = null;
+	}
+
+	function scan() {
+		cam.pause();
+		cropper.hidden = false;
+		crop_controls.hidden = false;
 	}
 
 	const saveAs = (data, filename = "untitled", type = "text/plain") => {
